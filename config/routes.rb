7118493ae3d59e7e49b62b0fcf70f16c60234639
vagrant <vagrant@localhost.localdomain>
2020-11-18 16:19:22 +0000

    add home&about controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  root to: 'homes#top'
+
+  get '/home/about' => 'homes#about'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
